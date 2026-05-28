f63832194c13ec9ca377cb7480f81f624bcd240e
SilviuASY <160680659+SilviuASY@users.noreply.github.com>
2026-05-28 19:47:47 +0200

    Update wagmi.ts

--- src/wagmi.ts
+++ src/wagmi.ts
@@ -13,60 +13,14 @@ export const soneiumChain = {
   iconUrl: '/soneium.png',
 } as const
 
-export const inkChain = {
-  id: 57073,
-  name: 'Ink',
-  nativeCurrency: { name: 'Ether', symbol: 'ETH', decimals: 18 },
-  rpcUrls: {
-    default: { http: ['https://rpc-qnd.inkonchain.com'] },
-    public: { http: ['https://rpc-qnd.inkonchain.com'] },
-  },
-  iconUrl: '/ink.png',
-} as const
 
-export const optimismChain = {
-  id: 10,
-  name: 'Optimism',
-  nativeCurrency: { name: 'Ether', symbol: 'ETH', decimals: 18 },
-  rpcUrls: {
-    default: { http: ['https://optimism-public.nodies.app'] },
-    public: { http: ['https://optimism-public.nodies.app'] },
-  },
-  iconUrl: '/optimism.png',
-} as const
-
-export const baseChain = {
-  id: 8453,
-  name: 'Base',
-  nativeCurrency: { name: 'Ether', symbol: 'ETH', decimals: 18 },
-  rpcUrls: {
-    default: { http: ['https://base-public.nodies.app'] },
-    public: { http: ['https://base-public.nodies.app'] },
-  },
-  iconUrl: '/base.png',
-} as const
-
-export const unichainChain = {
-  id: 130,
-  name: 'Unichain',
-  nativeCurrency: { name: 'Ether', symbol: 'ETH', decimals: 18 },
-  rpcUrls: {
-    default: { http: ['https://unichain-rpc.publicnode.com'] },
-    public: { http: ['https://unichain-rpc.publicnode.com'] },
-  },
-  iconUrl: '/unichain.png',
-} as const
 
 // Configurația principală pentru RainbowKit și Wagmi
 export const config = getDefaultConfig({
   appName: 'Agent GM Protocol',
   projectId: 'f46144873867897ce8bde287cdf40e46',
-  chains: [soneiumChain, inkChain, optimismChain, baseChain, unichainChain],
+  chains: [soneiumChain],
   transports: {
     [soneiumChain.id]: http(),
-    [inkChain.id]: http(),
-    [optimismChain.id]: http(),
-    [baseChain.id]: http(),
-    [unichainChain.id]: http(),
   },
 })
